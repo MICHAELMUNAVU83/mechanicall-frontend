3bc1d534f5fc2cf744817f3647b4cf4d686c2f12
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-20 14:16:56 +0300

    Changed layout

---
+++ src/components/FuelStations.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useContext, useState } from "react";
+import { UserContext } from "../context";
+function FuelStations() {
+  const { countyName, showPosition } = useContext(UserContext);
+  const [fuelStations, setFuelStations] = useState([]);
+  useEffect(() => {
+    showPosition();
+    fetch("/api/v1/fuel_stations", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setFuelStations(data));
+  }, [showPosition]);
+
+  return (
+    <div>
+      FuelStations
+      {fuelStations.map(
+        (fuelStation) =>
+          fuelStation.county === countyName && (
+            <div key={fuelStation.id}>
+              <h3>{fuelStation.name}</h3>
+            </div>
+          )
+      )}
+    </div>
+  );
+}
+
+export default FuelStations;
