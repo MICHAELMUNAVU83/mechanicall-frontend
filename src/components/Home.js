8a98f58fe4df69e762e7601d8741597136b3d0f5
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-23 18:51:19 +0300

    Added mapbox

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,47 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { UserContext } from "../context";
 import { Link } from "react-router-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, {
+  Marker,
+  NavigationControl,
+  GeolocateControl,
+  FullscreenControl,
+  Popup,
+} from "react-map-gl";
 import "./Home.css";
 function Home() {
-  const { setStoredToken, fetchProfile, name, showPosition, countyName } =
-    useContext(UserContext);
+  const [mechanics, setMechanics] = useState([]);
+  const {
+    setStoredToken,
+    fetchProfile,
+    name,
+    showPosition,
+    countyName,
+    latitude,
+    longitude,
+  } = useContext(UserContext);
+
+  console.log(latitude, longitude);
 
   useEffect(() => {
     fetchProfile();
     showPosition();
-  });
+    fetch("/api/v1/mechanics", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setMechanics(data));
+  }, [showPosition]);
 
   return (
     <div className="home">
       <div className="home__container" style={{ color: "white" }}>
-        <h1>WELCOME TO THE CAR SERVICE APP</h1>
+        <h1>HELLO {name.toUpperCase()} WELCOME TO THE CAR SERVICE APP</h1>
         <h2>
           Find the best mechanics and fuel stations near you in {countyName}
         </h2>
@@ -28,6 +55,58 @@ function Home() {
             </button>
           </Link>
         </div>
+        <Map
+          style={{
+            margin: "auto",
+            width: "500px",
+            height: "500px",
+            border: "2px solid black",
+            borderRadius: "5px",
+          }}
+          initialViewState={{
+            latitude: -1.1075584,
+            longitude: 37.027885,
+            zoom: 10,
+          }}
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN} // This is the token we got from Mapbox
+          mapStyle="mapbox://styles/mapbox/streets-v11" // This is the style of the map
+        >
+          <NavigationControl showCompass={true} position="bottom-right" />
+          {mechanics.map((restaurant) => (
+            <Marker
+              color="blue"
+              key={restaurant.id}
+              latitude={restaurant.latitude}
+              longitude={restaurant.longitude}
+            >
+              <i
+                className="fa-solid fa-car"
+                onClick={(e) => {
+                  alert(restaurant.name);
+                }}
+              ></i>
+            </Marker>
+          ))}
+          {/* {selectedRestaurant ? (
+            <Popup
+              latitude={selectedRestaurant.latitude}
+              longitude={selectedRestaurant.longitude}
+              onClose={() => {
+                setSelectedRestaurant(null);
+              }}
+            >
+              <div>{selectedRestaurant.name}</div>
+            </Popup>
+          ) : null} */}
+
+          <GeolocateControl
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+            showUserLocation={true}
+          />
+
+          <FullscreenControl container={document.querySelector("body")} />
+        </Map>
       </div>
 
       {/* Hello {name}
